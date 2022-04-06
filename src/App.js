20a2622bb90687df53117ebacc98c848ca9075ae
klauslube <klauslube93@gmail.com>
2022-04-06 14:58:37 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
-  render() {s
-    return (<p>Sistema Solar</p>);
+  render() {
+    return (<Header />);
   }
 }
 
